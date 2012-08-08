060cf1954e2f4b4bceafe9f4bea7f82151d826f9
Kim Dong-il <eastkiki@hanmail.net>
2012-08-08 15:31:24 +0900

    Modify build system ( used grunt )

--- build.js
+++ build.js
@@ -1,87 +1,12 @@
-/*jshint regexp: false, node: true */
-(function () {
-    'use strict';
+var path = require("path"),
+    exec = require("child_process").exec;
 
-    var async = require('async'),
-        clc = require('cli-color'),
-        compressor = require('node-minify'),
-        exec = require('child_process').exec;
-
-    function success() {
-        console.log(clc.green(Array.prototype.join.call(arguments, ' ')));
+var command = "./node_modules/.bin/grunt";
+exec(path.normalize(command), function (err, stdout, stderr) {
+    if (err && !stderr) {
+        console.log(err.stack, err);
     }
 
-    function error() {
-        console.error(clc.red(Array.prototype.join.call(arguments, ' ')));
-    }
-    var destFileName = "userAgent.min.js";
-    var srcFileName = "userAgent.js";
-    var build = exports.upload = function (callback) {
-
-        // step 1. check code by jshint
-        function checkJsHint (cb) {
-            var command = "jshint src/js/userAgent.js";
-            exec(command, function (err, stdout, stderr) {
-                if (err) {
-                    console.log(stderr);
-                    cb(err);
-                } else {
-                    console.log(stdout);
-                    success("JsHint PASS!!!\n\n");
-                    cb(null);
-                }
-            });
-        }
-
-        // step 2. check test code by jsamine
-        function checkTestCase (cb) {
-            var command = "jasmine-node src/test-js/userAgent_spec.js";
-            exec(command, function (err, stdout, stderr) {
-                if (err) {
-                    console.log(stderr);
-                    cb(err);
-                } else {
-                    console.log(stdout);
-                    success("Test Case PASS!!!\n\n");
-                    cb(null);
-                }
-            });
-        }
-
-        // step 3. minify source file
-        function minifyFile(cb) {
-            try {
-                new compressor.minify({
-                    type: 'gcc',
-                    fileIn: __dirname + "/src/js/"+srcFileName,
-                    fileOut: __dirname + "/dist/"+destFileName,
-                    callback: function(err){
-                        if (err) {
-                            error("error : Minify " + srcFileName + " to " + destFileName + "\n\n");
-                            cb(err);
-                        } else {
-                            success("success : Minify " + srcFileName + " to " + destFileName + "\n\n");
-                            cb(null);
-                        }
-                    }
-                });
-            } catch (err) {
-                cb(err);
-            }
-        }
-
-        async.waterfall([
-            checkJsHint,
-            checkTestCase,
-            minifyFile
-        ], callback);
-    };
-
-    build(function(err){
-        if (err) {
-            error("error :");
-            console.error(err.stack);
-        }
-        success("Success Build!!");
-    });
-})();
+    console.log(stderr);
+    console.log(stdout);
+});
