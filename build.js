54479fa52299f6ec2fdd4940706bb12fed91a6cd
Kim Dong-il <eastkiki@hanmail.net>
2012-09-03 16:15:11 +0900

    add license

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 var path = require("path"),
     exec = require("child_process").exec;
 
-var command = "./node_modules/.bin/grunt";
+var command = "./node_modules/.bin/grunt -v";
 exec(path.normalize(command), function (err, stdout, stderr) {
     if (err && !stderr) {
         console.log(err.stack, err);
